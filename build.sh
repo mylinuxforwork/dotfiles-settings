a8e6632740cd164a36a799f4c9cbe4f3721a3d7f
Stephan Raabe <mail@stephan-raabe.de>
2024-04-25 18:17:01 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 cp usr/bin/restore.py ~/dotfiles-versions/dotfiles/.install/
 echo ":: restore.py copied to ~/dotfiles-versions/dotfiles/.install/"
+
+rm -rf ~/tmp
+mkdir ~/tmp
+cp -r . ~/tmp
+rm -rf ~/tmp/.git
+rm -rf ~/tmp/release
+rm -rf ~/tmp/screenshots
+rm ~/tmp/.gitignore
+rm ~/tmp/build.sh
+rm ~/tmp/install.sh
 cd ..
-ARCH=x86_64 appimagetool ml4w-dotfiles-settings
+ARCH=x86_64 appimagetool tmp
 echo ":: AppImage created"
+cp ML4W_Dotfiles_Settings-x86_64.AppImage ~/ml4w-dotfiles-settings/release/
 cp ML4W_Dotfiles_Settings-x86_64.AppImage ~/dotfiles-versions/dotfiles/apps/
 echo ":: AppImage copied to ~/dotfiles-versions/dotfiles/apps/"
