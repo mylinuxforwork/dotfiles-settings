c8f400d0df336a7deaef0442f85bf9c2c371cbcc
Stephan Raabe <mail@stephan-raabe.de>
2025-03-14 08:52:37 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with Packages Installer 0.1
+# Created with Packages Installer 0.5
 # https://github.com/mylinuxforwork/packages-installer
 
 clear
@@ -15,7 +15,7 @@ _space() {
 
 # Default
 assumeyes=1
-cmdoutput="/dev/null 2>&1"
+cmdoutput=1
 
 # Options
 while getopts y?h?o? option
@@ -26,10 +26,10 @@ do
 	        assumeyes=0
         	;;
         o|\?)
-	        cmdoutput="echo"
+	        cmdoutput=0
         	;;
         h|\?)
-		echo "Created with Packages Manager 0.1"
+		echo "Created with Packages Installer"
 		echo
 		echo "Usage:"
 		echo "-y Skip confirmation"
@@ -55,21 +55,7 @@ _isInstalled_flatpak() {
 }
 
 # Add flathub remote
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-
-_isInstalled_flatpak() {
-	package="$1"
-	check=$(flatpak info ${package})
-	if [[ $check == *"ID:"* ]]; then
-	  	echo 0
-	else
-		echo 1
-	fi
-}
-
-# Add flathub remote
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo > /dev/null 2>&1
 
 
 # Header
@@ -77,7 +63,7 @@ _sep
 echo "Setup ML4W Dotfiles Settings"
 echo "Remote Installation of Flatpak"
 _space
-echo "Created with Packages Manager 0.1"
+echo "Created with Packages Installer"
 _sep
 _space
 echo "IMPORTANT: Please make sure that your system is updated before starting the installation."
@@ -108,13 +94,38 @@ sudo -v
 _space
 
 # Packages
+check_isinstalled="True"
+if [ $check_isinstalled == "true" ]; then
+	if [[ $(_isInstalled_apt "org.gnome.Platform/x86_64/47") == 0 ]]; then
+		echo ":: org.gnome.Platform/x86_64/47 is already installed"
+	else
+		echo ":: Installing org.gnome.Platform/x86_64/47..."
+		if [ $cmdoutput == 1 ]; then
+			flatpak -y install "org.gnome.Platform/x86_64/47" > /dev/null 2>&1
+		else
+			flatpak -y install "org.gnome.Platform/x86_64/47"
+		fi
+	fi
+else
+	echo ":: Installing org.gnome.Platform/x86_64/47..."
+	if [ $cmdoutput == 1 ]; then
+		flatpak -y --reinstall install "org.gnome.Platform/x86_64/47" > /dev/null 2>&1
+	else
+		flatpak -y --reinstall install "org.gnome.Platform/x86_64/47"
+	fi
+fi
+
 if [ ! -d $HOME/.cache ]; then
 	mkdir -p $HOME/.cache
 fi
 wget -q -P "$HOME/.cache" "https://github.com/mylinuxforwork/dotfiles-settings/releases/latest/download/com.ml4w.settings.flatpak"
 cd "$HOME/.cache"
 echo ":: Installing com.ml4w.settings.flatpak"
-eval "flatpak --user -y --reinstall install com.ml4w.settings.flatpak > $cmdoutput"
+if [ $cmdoutput == 1 ]; then
+	flatpak --user -y --reinstall install com.ml4w.settings.flatpak > /dev/null 2>&1
+else
+	flatpak --user -y --reinstall install com.ml4w.settings.flatpak
+fi
 rm "$HOME/.cache/com.ml4w.settings.flatpak"
 
 
